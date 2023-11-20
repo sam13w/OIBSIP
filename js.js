23684814f1887681d22e78d23022efeced8fb629
sam13w <151004184+sam13w@users.noreply.github.com>
2023-11-20 23:34:41 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,23 @@
+
+function imgSlider(anything){
+    document.querySelector('#star');
+    star.src = anything;
+ }
+
+ function changecirclecolor(color){
+     const circle = document.querySelector('.circle');
+     circle.style.background=color;
+ }
+ function h2color(colo){
+     const h2 = document.querySelector('.h2');
+     h2.style.color=colo;
+ }
+ function ch(clor){
+     const circle2 = document.querySelector('.circle2');
+     circle2.style.background=clor;
+ } 
+ function text1(change) {
+     const tex = document.querySelector('.tex');
+     tex.textContent = change;
+ }  
+ 
